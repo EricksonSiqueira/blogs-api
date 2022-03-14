c2e3806854c1bda1ea283c498438046ed16e65d7
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-14 15:32:55 -0300

    feat: cria conxexão express pra rotar user

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const userRoute = require('./routes/user.routes');
+
+app.use(express.json());
+
+app.use('/user', userRoute);
+
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
