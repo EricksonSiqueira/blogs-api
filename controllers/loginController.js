9194686a97930c91a41b8b528e7b06bb6a04751c
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-14 16:17:57 -0300

    feat: controller do login

---
+++ controllers/loginController.js
@@ -0,0 +1,26 @@
+const { User } = require('../models');
+const generateToken = require('../utils/generateToken');
+
+const post = async (req, res, _next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email, password } });
+
+    if (user === null) {
+      return res.status(400).json({ message: 'Invalid fields' });
+    }
+
+    const token = generateToken(email, user.displayName);
+
+    return res.status(200).json({ token });
+  } catch (err) {
+    console.log(err);
+
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
+};
+
+module.exports = {
+  post,
+};
