fa73ece8e5db2842df48ac42b574dc3568ca2d80
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-14 15:33:10 -0300

    feat: modelo do user

---
+++ models/user.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: { type: DataTypes.STRING },
+    email: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    image: { type: DataTypes.STRING },
+  }, {
+    timestamps: false,
+    tableName: 'Users',
+  });
+
+  return User;
+};
