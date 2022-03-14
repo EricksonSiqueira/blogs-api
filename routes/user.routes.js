89219f93cdf78783a13f240420edea2616816575
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-14 16:57:41 -0300

    feat: rota get que pega todos os usuarios

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,11 +4,16 @@ const router = express.Router();
 
 const userController = require('../controllers/userController');
 const validator = require('../middlewares/userValidations');
+const tokenValidator = require('../middlewares/tokenValidation');
 
 router.post('/',
   validator.isDisplayNameValid,
   validator.isEmailValid,
   validator.isPasswordValid,
   userController.post);
+
+router.get('/', 
+  tokenValidator.isTokenValid,
+  userController.getAll);
 
 module.exports = router;
