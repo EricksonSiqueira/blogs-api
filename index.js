7f9afb5c64326fe1c89fca36d658404e59731d53
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-14 16:18:35 -0300

    feat: rota post /login

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 
 const userRoute = require('./routes/user.routes');
+const loginController = require('./controllers/loginController');
+const validation = require('./middlewares/loginValidations');
 
 app.use(express.json());
 
 app.use('/user', userRoute);
+
+app.post('/login',
+  validation.isEmailValid,
+  validation.isPasswordValid,
+  loginController.post);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
