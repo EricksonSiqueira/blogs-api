a0780e1c007e2d4f24f376a9c94aa74ae9414789
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-15 22:27:16 -0300

    feat: função getById do postController

--- controllers/postController.js
+++ controllers/postController.js
@@ -46,7 +46,31 @@ const getAll = async (req, res, _next) => {
   }
 };
 
+const getById = async (req, res, _next) => {
+  const { id } = req.params;
+  try {
+    const foundPost = await BlogPost.findOne(
+      {
+        where: { id },
+        include: [
+          { model: User, as: 'user' },
+          { model: Category, as: 'categories', through: { attributes: [] } },
+        ],
+      },
+    );
+
+    if (!foundPost) return res.status(404).json({ message: 'Post does not exist' });
+
+    return res.status(200).json(foundPost);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
+};
+
 module.exports = {
   post,
   getAll,
+  getById,
 };
