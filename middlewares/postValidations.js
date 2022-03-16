f1e8595849fb806eea169ad5df185405bb1abc1c
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-15 23:21:53 -0300

    feat: validações adicionais para rota PUT/post/:id

--- middlewares/postValidations.js
+++ middlewares/postValidations.js
@@ -1,4 +1,5 @@
-const { Category } = require('../models');
+const { Category, BlogPost, User } = require('../models');
+const jwtFuncs = require('../utils/jwt');
 
 const isTitleValid = (req, res, next) => {
   const { title } = req.body;
@@ -36,8 +37,39 @@ const isCategoryIdsValid = (req, res, next) => {
   next();
 };
 
+const isCategoryThere = (req, res, next) => {
+  const { categoryIds } = req.body;
+
+  if (categoryIds) {
+    return res.status(400).json({ message: 'Categories cannot be edited' });
+  }
+
+  next();
+};
+
+const isUserAuthorized = async (req, res, next) => {
+  const { id } = req.params;
+  const { authorization } = req.headers;
+
+  const { email } = jwtFuncs.validateToken(authorization);
+
+  const post = await BlogPost.findOne({ where: { id } });
+  const user = await User.findOne({ where: { email } });
+
+  const postUserId = post.dataValues.userId;
+  const tokenUserId = user.dataValues.id;
+
+  if (postUserId !== tokenUserId) {
+    return res.status(401).json({ message: 'Unauthorized user' });
+  }
+
+  next();
+};
+
 module.exports = {
   isTitleValid,
   isContentValid,
   isCategoryIdsValid,
+  isCategoryThere,
+  isUserAuthorized,
 };
