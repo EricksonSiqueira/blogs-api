be5e8c383077d61dfecc35f49662f52c82fe6587
Erickson <ericksonsiqueirasilva@gmail.com>
2022-03-16 19:59:40 -0300

    refactor: coloca tudo dentro do try

--- middlewares/tokenValidation.js
+++ middlewares/tokenValidation.js
@@ -3,17 +3,15 @@ const { validateToken } = require('../utils/jwt');
 const isTokenValid = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (authorization === undefined || authorization.length <= 2) {
-    return res.status(401).json({ message: 'Token not found' });
-  }
-
   try {
+    if (authorization === undefined || authorization.length <= 2) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
     validateToken(authorization);
+    next();
   } catch (error) {
     return res.status(401).json({ message: 'Expired or invalid token' });
   }
-
-  next();
 };
 
 module.exports = {
